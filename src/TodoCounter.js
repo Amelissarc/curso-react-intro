52ed033b274c90c4f905bedc33f7b85beee6c73b
Amelissarc <melissa.rinconft@gmail.com>
2023-07-10 12:12:38 -0300

    Creating basic structure ToDo

---
+++ src/TodoCounter.js
@@ -0,0 +1,12 @@
+
+function TodoCounter(props){
+    return (
+      <h1>
+        Haz completado
+        {props.completed} de
+        {props.total} TODOs
+      </h1>
+    );
+}
+
+export {TodoCounter};
